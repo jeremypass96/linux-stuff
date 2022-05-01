eafbd833820087a42b250641aa0b3f866bbd4f48
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2022-05-01 11:44:56 -0400

    Update zsh-setup.sh

--- zsh-setup.sh
+++ zsh-setup.sh
@@ -1,5 +1,5 @@
-#!/bin/bash 
-cp -v /home/$USER/linux-stuff/Dotfiles/.zshrc /home/$USER/.zshrc
-cp -v /home/$USER/.zshrc /etc/skel/.zshrc
+#!/bin/bash
 git clone https://github.com/zsh-users/zsh-autosuggestions.git /home/$USER/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /home/$USER/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+cp -v /home/$USER/linux-stuff/Dotfiles/.zshrc /home/$USER/.zshrc
+cp -v /home/$USER/.zshrc /etc/skel/.zshrc
