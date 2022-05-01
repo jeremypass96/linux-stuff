1d2400558af5263ae41d67a3c65d2816f4703352
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2022-05-01 11:22:59 -0400

    Update kubuntu-cleanup.sh

--- kubuntu-cleanup.sh
+++ kubuntu-cleanup.sh
@@ -54,11 +54,3 @@ nala install -y plasma-workspace-wallpapers
 
 # Cleanup systemd boot.
 ./cleanup-systemd-boot-linux.sh
-
-# Install and configure zsh.
-nala install -y zsh
-cp -v /home/$USER/linux-stuff/Dotfiles/.zshrc /home/$USER/.zshrc
-cp -v /home/$USER/.zshrc /etc/skel/.zshrc
-git clone https://github.com/zsh-users/zsh-autosuggestions.git /home/$USER/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /home/$USER/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
