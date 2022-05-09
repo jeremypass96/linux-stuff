52b0a42511182be947ecf490f8e26ef2400abe6a
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2022-05-09 10:12:43 -0400

    Update kubuntu-cleanup.sh

--- kubuntu-cleanup.sh
+++ kubuntu-cleanup.sh
@@ -7,9 +7,6 @@ echo "Please run this script as root! Thanks."
 exit
 fi
 
-# Update repos.
-apt update
-
 # Add Nala repo and install nala, a nicer and better apt frontend.
 echo "deb [arch=amd64] http://deb.volian.org/volian/ scar main" | tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list
 wget -qO - https://deb.volian.org/volian/scar.key | tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg > /dev/null
@@ -28,7 +25,7 @@ snap remove --purge firefox
 nala install -y micro neofetch vlc fonts-roboto fonts-roboto-hinted
 
 # Install the Brave browser.
-nala install apt-transport-https curl
+nala install -y apt-transport-https curl
 curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | tee /etc/apt/sources.list.d/brave-browser-release.list
 nala update ; nala install -y brave-browser
