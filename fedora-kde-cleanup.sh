9096262fac4848038840b41855bb4eb910b3be5a
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2022-05-09 11:01:55 -0400

    Update fedora-kde-cleanup.sh

--- fedora-kde-cleanup.sh
+++ fedora-kde-cleanup.sh
@@ -2,7 +2,7 @@
 # This shell script cleans up a Fedora KDE spin and removes unnecessary bloatware included with the operating system.
 
 # Checking to see if we're running as root.
-if [ $(id -u) -ne 0 ] ; then
+if [ $(id -u) -ne 0 ]; then
 echo "Please run this script as root! Thanks."
 exit
 fi
