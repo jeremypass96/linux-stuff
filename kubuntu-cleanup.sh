32c5973461ddabfe3c9acb1e04c92b5dd6cf08b0
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2022-05-31 22:29:41 -0400

    Update kubuntu-cleanup.sh

--- kubuntu-cleanup.sh
+++ kubuntu-cleanup.sh
@@ -34,7 +34,7 @@ nala update; nala install -y brave-browser
 wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | apt-key add -
 sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
 nala update
-nala install atom
+nala install -y atom
 
 # Configure micro.
 mkdir /home/$USER/.config/micro
