377f2d23337137b4ee3fa4b02557bf4551395462
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2022-06-01 02:03:05 -0400

    Create neofetch-setup.sh

---
+++ neofetch-setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir /home/$USER/.config/neofetch
+cp -v /home/$USER/linux-stuff/Dotfiles/config/neofetch/config.conf /home/$USER/.config/neofetch/config.conf
+chown $USER:$USER /home/$USER/.config/neofetch
+chown $USER:$USER /home/$USER/.config/neofetch/*
+sudo mkdir -p /etc/skel/.config/neofetch
+sudo cp -v /home/$USER/.config/neofetch/config.conf /etc/skel/.config/neofetch/
