00b9ec2b33411a1afbe1a4a633245b7d28ba941c
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2022-06-01 14:53:14 -0400

    Update atom-setup.sh

--- atom-setup.sh
+++ atom-setup.sh
@@ -1,3 +1,4 @@
- #!/bin/bash
- cp -rv /home/$USER/linux-stuff/Dotfiles/.atom /home/$USER/.atom
- sudo cp -rv /home/$USER/linux-stuff/Dotfiles/.atom /etc/skel/.atom
+#!/bin/bash
+apm install atom-ui-tweaks
+cp -rv /home/$USER/linux-stuff/Dotfiles/.atom /home/$USER/.atom
+sudo cp -rv /home/$USER/linux-stuff/Dotfiles/.atom /etc/skel/.atom
