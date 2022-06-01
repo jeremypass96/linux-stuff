aec90918160caa237af10cd41829fe5b41c60873
Jeremy Passarelli <jeremy.pass.101@aol.com>
2022-06-01 11:58:53 -0400

    Fixed (again).

--- atom-setup.sh
+++ atom-setup.sh
@@ -1,3 +1,3 @@
  #!/bin/bash
  cp -rv /home/$USER/linux-stuff/Dotfiles/.atom /home/$USER/.atom
- sudo cp -v /home/$USER/linux-stuff/Dotfiles/.atom /etc/skel/.atom
+ sudo cp -rv /home/$USER/linux-stuff/Dotfiles/.atom /etc/skel/.atom
