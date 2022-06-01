aeab9084e2ab38a6862a037b940893e6e6122cfa
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2022-05-31 22:31:46 -0400

    Update zsh-setup.sh

--- zsh-setup.sh
+++ zsh-setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 git clone https://github.com/zsh-users/zsh-autosuggestions.git /home/$USER/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /home/$USER/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 cp -v /home/$USER/linux-stuff/Dotfiles/.zshrc /home/$USER/.zshrc
