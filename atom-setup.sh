5820571ddd6f4503e74f3e63ec7cd302a80ffcc4
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2022-06-02 14:10:13 -0400

    Update atom-setup.sh

--- atom-setup.sh
+++ atom-setup.sh
@@ -2,5 +2,5 @@
 apm install atom-ui-tweaks
 mkdir /home/$USER/.atom
 sudo mkdir /etc/skel/.atom
-cp -v /home/$USER/linux-stuff/Dotfiles/.atom/config.cson /home/$USER/.atom/config.cson
-sudo cp -v /home/$USER/linux-stuff/Dotfiles/.atom/config.cson /etc/skel/.atom/config.cson
+cp -v /home/$USER/linux-stuff/Dotfiles/atom/config.cson /home/$USER/.atom/config.cson
+sudo cp -v /home/$USER/linux-stuff/Dotfiles/atom/config.cson /etc/skel/.atom/config.cson
