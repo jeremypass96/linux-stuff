c1b3cb24c92471a9de9c9e2c480c5060e8339514
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2022-09-12 02:14:27 -0400

    Delete atom-setup.sh

--- atom-setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-apm install atom-ui-tweaks
-mkdir /home/$USER/.atom
-sudo mkdir /etc/skel/.atom
-cp -v /home/$USER/linux-stuff/Dotfiles/atom/config.cson /home/$USER/.atom/config.cson
-sudo cp -v /home/$USER/linux-stuff/Dotfiles/atom/config.cson /etc/skel/.atom/config.cson
