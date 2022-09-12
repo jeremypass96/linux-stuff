e4ff9ff444875436897599c2e9689a8931d3b41c
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2022-09-12 17:38:57 -0400

    Update fedora-kde-cleanup.sh

--- fedora-kde-cleanup.sh
+++ fedora-kde-cleanup.sh
@@ -41,7 +41,7 @@ dnf install -y brave-browser
 clear
 
 # Install some useful software.
-dnf install -y neofetch vlc audacity-freeworld PackageKit-command-not-found
+dnf install -y neofetch vlc audacity-freeworld PackageKit-command-not-found duf btop
 
 clear
 
