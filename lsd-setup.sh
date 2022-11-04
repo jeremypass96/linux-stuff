a50a0f17ec2628fef581f99b40edc26bfaf988b9
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2022-11-04 10:27:46 -0400

    Create lsd-setup.sh

---
+++ lsd-setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir /home/$USER/.config/lsd
+cp -v /home/$USER/linux-stuff/Dotfiles/config/lsd/config.yaml /home/$USER/.config/lsd/config.yaml
+chown $USER:$USER /home/$USER/.config/lsd
+chown $USER:$USER /home/$USER/.config/lsd/*
+sudo mkdir -p /etc/skel/.config/lsd
+sudo cp -v /home/$USER/.config/lsd/config.yaml /etc/skel/.config/lsd/
+sudo mkdir -p /root/.config/lsd
+sudo cp -v /etc/skel/.config/lsd/config.yaml /root/.config/lsd/
