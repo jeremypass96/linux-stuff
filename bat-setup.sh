79360f44bf7b8bcbb1baf027de596d0ed5553d99
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2023-01-29 21:44:02 -0500

    Update bat-setup.sh

--- bat-setup.sh
+++ bat-setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 bat --generate-config-file
-sed -i s/#--theme="TwoDark"/--theme='"1337"'/g /home/$USER/.config/bat/config
+sed -i s/#--theme='"TwoDark"'/--theme='"1337"'/g /home/$USER/.config/bat/config
 sed -i s/#--italic-text=always/--italic-text=always/g /home/$USER/.config/bat/config
