967def29907fe0a4d94620ebb66909070e4a5a87
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2023-01-29 20:24:06 -0500

    Update alpine_linux-setup.sh

--- alpine_linux-setup.sh
+++ alpine_linux-setup.sh
@@ -63,7 +63,7 @@ continue
 fi
 
 # Install KDE.
-apk add plasma kde-applications-base oxygen-sounds kcalc kcharselect kdf kwalletmanager kdeconnect juk print-manager sweeper papirus-icon-theme elogind polkit-elogind polkit-openrc dbus
+apk add plasma kde-applications-base oxygen-sounds kcalc kcharselect kdf kwalletmanager kdeconnect juk print-manager sweeper papirus-icon-theme elogind polkit-elogind polkit-openrc
 
 # Clean package cache.
 apk cache clean
