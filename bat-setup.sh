5b601b5b1637bde03ed5ead6afbb28daa9f78536
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2023-02-22 13:31:40 -0500

    Update bat-setup.sh

--- bat-setup.sh
+++ bat-setup.sh
@@ -2,4 +2,4 @@
 bat --generate-config-file
 sed -i s/#--theme='"TwoDark"'/--theme='"1337"'/g /home/$USER/.config/bat/config
 sed -i s/#--italic-text=always/--italic-text=always/g /home/$USER/.config/bat/config
-sudo mkdir -p /etc/skel/.config/bat/config && sudo cp -v /home/$USER/.config/bat/config /etc/skel/.config/bat/config
+sudo mkdir -p /etc/skel/.config/bat && sudo cp -v /home/$USER/.config/bat/config /etc/skel/.config/bat
