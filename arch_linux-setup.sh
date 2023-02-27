9e2c4760719316976ab69b28f13fd9da39a56344
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2023-02-27 09:15:42 -0500

    Update arch_linux-setup.sh

--- arch_linux-setup.sh
+++ arch_linux-setup.sh
@@ -14,10 +14,10 @@ sudo sed -i '/ParallelDownloads = 15/ a\ILoveCandy\' /etc/pacman.conf
 
 # Rank mirrors.
 sudo pacman -S reflector --noconfirm
-sudo reflector --latest 100 --protocol https --sort rate --sort age --score 10 --save /etc/pacman.d/mirrorlist
+sudo reflector --latest 150 --protocol https --sort rate --sort age --score 15 --save /etc/pacman.d/mirrorlist
 # Congfigure reflector config file for systemd auto-update.
 sudo chmod o+w /etc/xdg/reflector/reflector.conf
-sudo sed -i 's/--latest 5/--latest 100'/g /etc/xdg/reflector/reflector.conf
+sudo sed -i 's/--latest 5/--latest 150'/g /etc/xdg/reflector/reflector.conf
 echo "" >> /etc/xdg/reflector/reflector.conf
 echo "# Sort the mirrors by highest score (--score)." >> /etc/xdg/reflector/reflector.conf
 echo "--score 15" >> /etc/xdg/reflector/reflector.conf
