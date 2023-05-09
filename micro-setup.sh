1e296540639086ec9f0a56fa9e04ac7509e4b89f
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2023-05-09 19:29:37 -0400

    Update micro-setup.sh

--- micro-setup.sh
+++ micro-setup.sh
@@ -7,3 +7,13 @@ sudo mkdir -p /etc/skel/.config/micro
 sudo cp -v /home/$USER/.config/micro/settings.json /etc/skel/.config/micro/
 sudo mkdir -p /root/.config/micro
 sudo cp -v /etc/skel/.config/micro/settings.json /root/.config/micro
+
+# Setup Catppuccin colors.
+cd
+git clone https://github.com/catppuccin/micro.git
+mkdir -p ~/.config/micro/colorschemes
+sudo mkdir -p /etc/skel/.config/micro/colorschemes
+sudo mkdir -p /root/.config/micro/colorschemes
+cp -v micro/src/*.micro ~/.config/micro/colorschemes
+sudo cp -v micro/src/*.micro /etc/skel/.config/micro/colorschemes
+sudo cp -v micro/src/*.micro /root/.config/micro/colorschemes
