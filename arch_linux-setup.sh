207539d76c7450d0ca181a23c52ad673e77b20a6
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2023-05-11 13:10:01 -0400

    Update arch_linux-setup.sh

--- arch_linux-setup.sh
+++ arch_linux-setup.sh
@@ -98,7 +98,7 @@ sudo pacman -S hwdetect --noconfirm
 yay -S brave-bin --noconfirm
 
 # Install fonts.
-yay -S ttf-poppins adobe-source-sans-fonts ttf-ibm-plex ttf-ms-fonts ttf-material-design-icons-desktop-git ttf-material-design-icons ttf-material-design-icons-git noto-fonts-emoji ttf-nerd-fonts-symbols-2048-em --noconfirm
+yay -S ttf-poppins adobe-source-sans-fonts ttf-ibm-plex ttf-ms-fonts ttf-material-design-icons-desktop-git ttf-material-design-icons ttf-material-design-icons-git noto-fonts-emoji ttf-nerd-fonts-symbols --noconfirm
 sudo ln -s /usr/share/fontconfig/conf.avail/09-autohint-if-no-hinting.conf /etc/fonts/conf.d
 
 # Configure nerd fonts for "lsd".
