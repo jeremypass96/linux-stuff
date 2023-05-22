175d63ffae0597bd019e0ea61900744e8685c351
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2023-05-22 12:28:38 -0400

    Update zsh-setup.sh

--- zsh-setup.sh
+++ zsh-setup.sh
@@ -3,6 +3,8 @@ sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O 
 git clone https://github.com/zsh-users/zsh-autosuggestions.git /home/$USER/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /home/$USER/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 cp -v /home/$USER/linux-stuff/Dotfiles/.zshrc /home/$USER/.zshrc
+
+# Setup Catppuccin colors.
 git clone https://github.com/catppuccin/zsh-syntax-highlighting.git
 cd zsh-syntax-highlighting/themes/
 sudo cp -v catppuccin_mocha-zsh-syntax-highlighting.zsh /etc/zsh
