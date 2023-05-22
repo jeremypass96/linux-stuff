5050d017017cd8b19a95a1c910bbd729ea2ce273
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2023-05-22 12:36:20 -0400

    Update zsh-setup.sh

--- zsh-setup.sh
+++ zsh-setup.sh
@@ -7,8 +7,25 @@ cp -v /home/$USER/linux-stuff/Dotfiles/.zshrc /home/$USER/.zshrc
 # Setup Catppuccin colors.
 git clone https://github.com/catppuccin/zsh-syntax-highlighting.git
 cd zsh-syntax-highlighting/themes/
-sudo cp -v catppuccin_mocha-zsh-syntax-highlighting.zsh /etc/zsh
+sudo cp -v *.zsh /etc/zsh
+read -p "Which Catppuccin colors do you want for syntax highlighting?
+1.) Latte
+2.) Frappé
+3.) Macchiato
+4.) Mocha
+-> " resp
+if [ "$resp" = 1 ]; then
+echo source /etc/zsh/catppuccin_latte-zsh-syntax-highlighting.zsh >> /home/$USER/.zshrc
+fi
+if [ "$resp" = 2 ]; then
+echo source /etc/zsh/catppuccin_frappe-zsh-syntax-highlighting.zsh >> /home/$USER/.zshrc
+fi
+if [ "$resp" = 3 ]; then
+echo source /etc/zsh/catppuccin_macchiato-zsh-syntax-highlighting.zsh >> /home/$USER/.zshrc
+fi
+if [ "$resp" = 4 ]; then
 echo source /etc/zsh/catppuccin_mocha-zsh-syntax-highlighting.zsh >> /home/$USER/.zshrc
+fi
 cd && rm -rf zsh-syntax-highlighting
 sudo cp -v /home/$USER/.zshrc /etc/skel/.zshrc
 sudo cp -v /etc/skel/.zshrc /root/.zshrc
