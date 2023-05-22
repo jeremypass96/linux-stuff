2e0ce730222d81949f8c15be54e0e1e0a4b28104
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2023-05-22 12:25:09 -0400

    Update zsh-setup.sh

--- zsh-setup.sh
+++ zsh-setup.sh
@@ -3,5 +3,11 @@ sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O 
 git clone https://github.com/zsh-users/zsh-autosuggestions.git /home/$USER/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /home/$USER/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 cp -v /home/$USER/linux-stuff/Dotfiles/.zshrc /home/$USER/.zshrc
+git clone https://github.com/catppuccin/zsh-syntax-highlighting.git
+cd zsh-syntax-highlighting/themes/
+sudo cp -v catppuccin_mocha-zsh-syntax-highlighting.zsh /etc/zsh
+echo source /etc/zsh/catppuccin_mocha-zsh-syntax-highlighting.zsh >> /home/$USER/.zshrc
+cd && rm -rf zsh-syntax-highlighting
 sudo cp -v /home/$USER/.zshrc /etc/skel/.zshrc
 sudo cp -v /etc/skel/.zshrc /root/.zshrc
+echo pfetch >> /home/$USER/.zshrc
