fbd72189ce6e2bfef1e7fd4bed985bd64db3653e
Jeremy Passarelli <jeremy.pass.101@aol.com>
2023-05-29 09:53:18 -0400

    Added a couple lines to bat setup script. Added syntax hylighting support for .conf files.

--- bat-setup.sh
+++ bat-setup.sh
@@ -2,5 +2,7 @@
 bat --generate-config-file
 sed -i s/#--theme='"TwoDark"'/--theme='"1337"'/g /home/$USER/.config/bat/config
 sed -i s/#--italic-text=always/--italic-text=always/g /home/$USER/.config/bat/config
+echo "--map-syntax "*.conf:INI"" >> /home/$USER/.config/bat/config
+echo "--map-syntax "config:INI"" >> /home/$USER/.config/bat/config
 sudo mkdir -p /etc/skel/.config/bat && sudo cp -v /home/$USER/.config/bat/config /etc/skel/.config/bat
 sudo mkdir -p /root/.config/bat && sudo cp -v /etc/skel/.config/bat/config /root/.config/bat/config
