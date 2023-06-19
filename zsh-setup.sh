8dcc3fc7bad6a98603ed74d62607f22da56d295b
Jeremy Passarelli <jeremy.pass.101@aol.com>
2023-06-19 17:07:43 -0400

    Fixed issues.

--- zsh-setup.sh
+++ zsh-setup.sh
@@ -5,9 +5,11 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /home/$USER/.
 cp -v /home/$USER/linux-stuff/Dotfiles/.zshrc /home/$USER/.zshrc
 
 # Setup Catppuccin colors.
+cd
 git clone https://github.com/catppuccin/zsh-syntax-highlighting.git
 cd zsh-syntax-highlighting/themes/
-sudo cp -v *.zsh /etc/zsh
+sudo mkdir -p /usr/local/etc/zsh
+sudo cp -v *.zsh /usr/local/etc/zsh
 read -p "Which Catppuccin colors do you want for syntax highlighting?
 1.) Latte
 2.) Frappé
@@ -15,16 +17,16 @@ read -p "Which Catppuccin colors do you want for syntax highlighting?
 4.) Mocha
 -> " resp
 if [ "$resp" = 1 ]; then
-echo source /etc/zsh/catppuccin_latte-zsh-syntax-highlighting.zsh >> /home/$USER/.zshrc
+echo source /usr/local/etc/zsh/catppuccin_latte-zsh-syntax-highlighting.zsh >> /home/$USER/.zshrc
 fi
 if [ "$resp" = 2 ]; then
-echo source /etc/zsh/catppuccin_frappe-zsh-syntax-highlighting.zsh >> /home/$USER/.zshrc
+echo source /usr/local/etc/zsh/catppuccin_frappe-zsh-syntax-highlighting.zsh >> /home/$USER/.zshrc
 fi
 if [ "$resp" = 3 ]; then
-echo source /etc/zsh/catppuccin_macchiato-zsh-syntax-highlighting.zsh >> /home/$USER/.zshrc
+echo source /usr/local/etc/zsh/catppuccin_macchiato-zsh-syntax-highlighting.zsh >> /home/$USER/.zshrc
 fi
 if [ "$resp" = 4 ]; then
-echo source /etc/zsh/catppuccin_mocha-zsh-syntax-highlighting.zsh >> /home/$USER/.zshrc
+echo source /usr/local/etc/zsh/catppuccin_mocha-zsh-syntax-highlighting.zsh >> /home/$USER/.zshrc
 fi
 cd && rm -rf zsh-syntax-highlighting
 sudo cp -v /home/$USER/.zshrc /etc/skel/.zshrc
