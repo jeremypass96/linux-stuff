9fbd5aa1ba9e0706fa4875e67d14bfd6f5617812
Jeremy Passarelli <jeremy.pass.101@aol.com>
2023-06-20 19:06:22 -0400

    Removed font package that no longer exists in the AUR.

--- arch_linux-setup.sh
+++ arch_linux-setup.sh
@@ -105,7 +105,7 @@ sudo pacman -S hwdetect --noconfirm
 paru -S brave-bin --noconfirm
 
 # Install fonts.
-paru -S ttf-poppins adobe-source-sans-fonts ttf-jetbrains-mono-nerd ttf-ms-fonts ttf-material-design-icons-desktop-git ttf-material-design-icons ttf-material-design-icons-git noto-fonts-emoji ttf-nerd-fonts-symbols --noconfirm
+paru -S ttf-poppins adobe-source-sans-fonts ttf-jetbrains-mono-nerd ttf-ms-fonts ttf-material-design-icons-desktop-git ttf-material-design-icons-git noto-fonts-emoji ttf-nerd-fonts-symbols --noconfirm
 sudo ln -s /usr/share/fontconfig/conf.avail/09-autohint-if-no-hinting.conf /etc/fonts/conf.d/
 
 # Configure nerd fonts for "lsd".
