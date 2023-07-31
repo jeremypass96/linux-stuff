3db1211e42fdf219a3f65c45d6fee9dda414c9ef
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2023-07-30 20:35:49 -0400

    Update bat-setup.sh

--- bat-setup.sh
+++ bat-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 bat --generate-config-file
-sed -i s/#--theme="'TwoDark'"/--theme="'1337'"/g /home/$USER/.config/bat/config
+sed -i s/#--theme='"TwoDark"'/--theme='"1337"'/g /home/$USER/.config/bat/config
 sed -i s/#--italic-text=always/--italic-text=always/g /home/$USER/.config/bat/config
 echo "--map-syntax "*.conf:INI"" >> /home/$USER/.config/bat/config
 echo "--map-syntax "config:INI"" >> /home/$USER/.config/bat/config
