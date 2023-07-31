e2220c6a9e0b36bf301d2a76f7553a5a6b19f3a9
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2023-07-30 20:30:53 -0400

    Update bat-setup.sh

--- bat-setup.sh
+++ bat-setup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 bat --generate-config-file
-sed -i s/#--theme='"TwoDark"'/--theme='"1337"'/g /home/$USER/.config/bat/config
+sed -i s/#--theme/--theme/g /home/$USER/.config/bat/config
 sed -i s/#--italic-text=always/--italic-text=always/g /home/$USER/.config/bat/config
 echo "--map-syntax "*.conf:INI"" >> /home/$USER/.config/bat/config
 echo "--map-syntax "config:INI"" >> /home/$USER/.config/bat/config
 sudo mkdir -p /etc/skel/.config/bat && sudo cp -v /home/$USER/.config/bat/config /etc/skel/.config/bat
 sudo mkdir -p /root/.config/bat && sudo cp -v /etc/skel/.config/bat/config /root/.config/bat/config
+
+# Setup the Catppuccin "mocha" theme for bat.
+cd && git clone https://github.com/catppuccin/bat.git
+mkdir -p "$(bat --config-dir)/themes"
+cp *.tmTheme "$(bat --config-dir)/themes"
+bat cache --build
+sed -i 's/TwoDark/Catppuccin-mocha'/g /home/$USER/.config/bat/config
