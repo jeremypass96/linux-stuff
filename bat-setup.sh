14cfaacf5638533a8404ff783e27c527e8d93836
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2023-08-08 18:56:51 -0400

    Update bat-setup.sh

--- bat-setup.sh
+++ bat-setup.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
-bat --generate-config-file
-sed -i s/#--theme='"TwoDark"'/--theme='"1337"'/g /home/$USER/.config/bat/config
-sed -i s/#--italic-text=always/--italic-text=always/g /home/$USER/.config/bat/config
-echo "--map-syntax "*.conf:INI"" >> /home/$USER/.config/bat/config
-echo "--map-syntax "config:INI"" >> /home/$USER/.config/bat/config
-sudo mkdir -p /etc/skel/.config/bat && sudo cp -v /home/$USER/.config/bat/config /etc/skel/.config/bat
-sudo mkdir -p /root/.config/bat && sudo cp -v /etc/skel/.config/bat/config /root/.config/bat/config
 
-# Setup the Catppuccin "mocha" theme for bat.
-cd && git clone https://github.com/catppuccin/bat.git
+# Generate initial configuration file for bat
+bat --generate-config-file
+
+# Modify the configuration settings
+sed -i 's/#--theme="TwoDark"/--theme="1337"/g' "$HOME/.config/bat/config"
+sed -i 's/#--italic-text=always/--italic-text=always/g' "$HOME/.config/bat/config"
+echo '--map-syntax "*.conf:INI"' >> "$HOME/.config/bat/config"
+echo '--map-syntax "config:INI"' >> "$HOME/.config/bat/config"
+
+# Copy the user configuration to /etc/skel so new users get the same setup
+sudo mkdir -p /etc/skel/.config/bat
+sudo cp -v "$HOME/.config/bat/config" /etc/skel/.config/bat/
+
+# Copy the user configuration to root's configuration
+sudo mkdir -p /root/.config/bat
+sudo cp -v "$HOME/.config/bat/config" /root/.config/bat/
+
+# Setup the Catppuccin "mocha" theme for bat
+cd "$HOME" || exit
+git clone https://github.com/catppuccin/bat.git
 cd bat
-mkdir -p "$(bat --config-dir)/themes"
-cp *.tmTheme "$(bat --config-dir)/themes"
-bat cache --build
-sed -i 's/1337/Catppuccin-mocha'/g /home/$USER/.config/bat/config
+sudo sh -c 'mkdir -p "$(bat --config-dir)/themes"; cp *.tmTheme "$(bat --config-dir)/themes"; bat cache --build; sed -i "s/1337/Catppuccin-mocha/g" "$HOME/.config/bat/config"'
+
+echo "Bat syntax highlighter has been configured with the Catppuccin 'mocha' theme for both your user and root."
+rm -rf $HOME/bat
+sudo rm -rf root/bat
