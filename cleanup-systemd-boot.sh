25eabddcca619c4aa8721d81b5ae9b1e85d7d3c3
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2023-08-08 19:09:55 -0400

    Update cleanup-systemd-boot.sh
    Cleaned up script generated and updated by ChatGPT.

--- cleanup-systemd-boot.sh
+++ cleanup-systemd-boot.sh
@@ -2,20 +2,30 @@
 # This script cleans up and makes bootup faster.
 
 # Checking to see if we're running as root.
-if [ $(id -u) -ne 0 ]; then
-echo "Please run this script as root! Thanks."
-exit
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Please run this script as root! Thanks."
+    exit 1
 fi
 
-systemctl disable NetworkManager-wait-online.service
-systemctl disable pppd-dns.service
-systemctl disable wpa_supplicant.service
-systemctl mask lvm2-monitor.service
-systemctl disable systemd-udev-settle.service
-systemctl stop bluetooth.service
-systemctl disable bluetooth.service
-systemctl disable brltty.service
-systemctl disable debug-shell.service
-systemctl disable ModemManager.service
+services_to_disable=(
+    NetworkManager-wait-online.service
+    pppd-dns.service
+    wpa_supplicant.service
+    lvm2-monitor.service
+    systemd-udev-settle.service
+    bluetooth.service
+    brltty.service
+    debug-shell.service
+    ModemManager.service
+)
 
-echo "Services have been disabled and stopped to help improve bootup speed."
+for service in "${services_to_disable[@]}"; do
+    if [ "$service" = "lvm2-monitor.service" ]; then
+        systemctl mask "$service"
+    else
+        systemctl disable "$service"
+        systemctl stop "$service"
+    fi
+done
+
+echo "Services have been disabled and stopped (except lvm2-monitor.service, which has been masked) to help improve bootup speed."
