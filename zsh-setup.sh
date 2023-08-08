4e67ad9c8d229723c77ae24f137edbfb1a4ae778
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2023-08-08 19:44:23 -0400

    Update zsh-setup.sh
    Cleaned up script generated and updated by ChatGPT.

--- zsh-setup.sh
+++ zsh-setup.sh
@@ -1,35 +1,56 @@
 #!/bin/bash
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-git clone https://github.com/zsh-users/zsh-autosuggestions.git /home/$USER/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /home/$USER/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-cp -v /home/$USER/linux-stuff/Dotfiles/.zshrc /home/$USER/.zshrc
 
-# Setup Catppuccin colors.
-cd
+# Install Oh My Zsh and change shell to Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+chsh -s /usr/bin/zsh $SUDO_USER
+
+# Clone and set up zsh-autosuggestions and zsh-syntax-highlighting plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions.git /home/$SUDO_USER/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /home/$SUDO_USER/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
+# Copy .zshrc configuration
+cp -v /home/$SUDO_USER/linux-stuff/Dotfiles/.zshrc /home/$SUDO_USER/.zshrc
+
+# Setup Catppuccin syntax highlighting colors
+cd "$HOME" || exit
 git clone https://github.com/catppuccin/zsh-syntax-highlighting.git
 cd zsh-syntax-highlighting/themes/
 sudo mkdir -p /usr/local/etc/zsh
 sudo cp -v *.zsh /usr/local/etc/zsh
-sudo chown $USER:$USER /usr/local/etc/zsh/*.zsh
-read -p "Which Catppuccin colors do you want for syntax highlighting?
-1.) Latte
-2.) Frappé
-3.) Macchiato
-4.) Mocha
--> " resp
-if [ "$resp" = 1 ]; then
-echo source /usr/local/etc/zsh/catppuccin_latte-zsh-syntax-highlighting.zsh >> /home/$USER/.zshrc
-fi
-if [ "$resp" = 2 ]; then
-echo source /usr/local/etc/zsh/catppuccin_frappe-zsh-syntax-highlighting.zsh >> /home/$USER/.zshrc
-fi
-if [ "$resp" = 3 ]; then
-echo source /usr/local/etc/zsh/catppuccin_macchiato-zsh-syntax-highlighting.zsh >> /home/$USER/.zshrc
-fi
-if [ "$resp" = 4 ]; then
-echo source /usr/local/etc/zsh/catppuccin_mocha-zsh-syntax-highlighting.zsh >> /home/$USER/.zshrc
-fi
+sudo chown $SUDO_USER:$SUDO_USER /usr/local/etc/zsh/*.zsh
+
+# Choose Catppuccin color scheme for syntax highlighting
+echo "Choose Catppuccin colors for syntax highlighting:"
+select scheme in "Latte" "Frappé" "Macchiato" "Mocha"; do
+    case $scheme in
+        Latte)
+            echo "source /usr/local/etc/zsh/catppuccin_latte-zsh-syntax-highlighting.zsh" >> /home/$SUDO_USER/.zshrc
+            break
+            ;;
+        Frappé)
+            echo "source /usr/local/etc/zsh/catppuccin_frappe-zsh-syntax-highlighting.zsh" >> /home/$SUDO_USER/.zshrc
+            break
+            ;;
+        Macchiato)
+            echo "source /usr/local/etc/zsh/catppuccin_macchiato-zsh-syntax-highlighting.zsh" >> /home/$SUDO_USER/.zshrc
+            break
+            ;;
+        Mocha)
+            echo "source /usr/local/etc/zsh/catppuccin_mocha-zsh-syntax-highlighting.zsh" >> /home/$SUDO_USER/.zshrc
+            break
+            ;;
+        *)
+            echo "Invalid selection. Please choose a valid option."
+            ;;
+    esac
+done
+
+# Clean up and copy configurations for future users and root
 cd && rm -rf zsh-syntax-highlighting
-sudo cp -v /home/$USER/.zshrc /etc/skel/.zshrc
+sudo cp -v /home/$SUDO_USER/.zshrc /etc/skel/.zshrc
 sudo cp -v /etc/skel/.zshrc /root/.zshrc
-echo pfetch >> /home/$USER/.zshrc
+
+# Add 'pfetch' to .zshrc
+echo "pfetch" >> /home/$SUDO_USER/.zshrc
+
+echo "Zsh and related configurations have been set up with the chosen Catppuccin syntax highlighting."
