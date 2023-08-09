ef2edc98d92c8d7497a390528769fe8e2c87f961
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2023-08-08 21:36:41 -0400

    Update fedora-kde-cleanup.sh
    Cleaned up script generated and updated by ChatGPT.

--- fedora-kde-cleanup.sh
+++ fedora-kde-cleanup.sh
@@ -3,8 +3,8 @@
 
 # Checking to see if we're running as root.
 if [ $(id -u) -ne 0 ]; then
-echo "Please run this script as root! Thanks."
-exit
+    echo "Please run this script as root! Thanks."
+    exit
 fi
 
 clear
@@ -12,18 +12,14 @@ clear
 # Audio buzz/hum fix.
 echo "options snd-hda-intel power_save=0 power_save_controller=N" >> /etc/modprobe.d/alsa-base.conf
 
-# Make DNF faster.
-echo fastestmirror=True >> /etc/dnf/dnf.conf
-echo max_parallel_downloads=20 >> /etc/dnf/dnf.conf
-
-# Sane DNF defaults.
-echo defaultyes=True >> /etc/dnf/dnf.conf
-echo install_weak_deps=False >> /etc/dnf/dnf.conf
+# Configure DNF for speed and sanity.
+echo -e "fastestmirror=True\nmax_parallel_downloads=20\ndefaultyes=True\ninstall_weak_deps=False" >> /etc/dnf/dnf.conf
 
 # Remove bloatware and install some KDE games.
-dnf remove -y libreoffice-* kaddressbook kmail kontact elisa-player kamoso kgpg kmag kmouth kmahjongg qt5-qdbusviewer firefox dragon krdc krfb kolourpaint akregator im-chooser korganizer dnfdragora kmousetool mediawriter && dnf install -y kapman kbrickbuster kblocks kbounce knetwalk
+dnf remove -y libreoffice-* kaddressbook kmail kontact elisa-player kamoso kgpg kmag kmouth kmahjongg qt5-qdbusviewer firefox dragon krdc krfb kolourpaint akregator im-chooser korganizer dnfdragora kmousetool mediawriter
+dnf install -y kapman kbrickbuster kblocks kbounce knetwalk
 
-# Install KDE Plasma browser integration (gets removed for some reason when removing bloatware).
+# Install KDE Plasma browser integration.
 dnf install -y plasma-browser-integration
 
 # Update Fedora install.
@@ -39,9 +35,8 @@ clear
 # Install Newaita icons.
 echo "Installing the Newaita icon theme..."
 cd && git clone https://github.com/cbrnix/Newaita.git
-cd Newaita/
-cp -r Newaita-dark /usr/share/icons
-cd && rm -rf Newaita
+cp -r Newaita/Newaita-dark /usr/share/icons
+rm -rf Newaita
 
 clear
 
@@ -70,7 +65,7 @@ dnf install -y k3b cdrskin sox
 clear
 
 # Install the micro text editor and remove nano.
-dnf install -y micro xclip ; dnf remove -y nano
+dnf install -y micro xclip && dnf remove -y nano
 
 clear
 
@@ -79,7 +74,7 @@ dnf install -y jetbrains-mono-fonts
 
 clear
 
-# Install KDE wallapers.
+# Install KDE wallpapers.
 dnf install -y plasma-workspace-wallpapers
 
 clear
@@ -97,9 +92,9 @@ clear
 # Download Konsole colors.
 cd && git clone https://github.com/catppuccin/konsole.git
 mkdir -p /home/$USER/.local/share/konsole/
-cd konsole/ && cp -v *.colorscheme /home/$USER/.local/share/konsole/
+cp -v konsole/*.colorscheme /home/$USER/.local/share/konsole/
 chown -R $USER:$USER /home/$USER/.local/share/konsole/
-cd && rm -rf konsole
+rm -rf konsole
 
 clear
 
@@ -130,32 +125,39 @@ dnf install -y lynis
 clear
 
 # Secure the OS.
-sed -i 117s/'022'/'077'/g /etc/login.defs
-sed -i s/'umask 022'/'umask 077'/g /etc/init.d/functions
-sed -i s/'umask 022'/'umask 077'/g /etc/bashrc
-sed -i s/'umask 022'/'umask 077'/g /etc/csh.cshrc
+declare -a files_to_secure=("/etc/login.defs" "/etc/init.d/functions" "/etc/bashrc" "/etc/csh.cshrc")
+for file in "${files_to_secure[@]}"; do
+    sed -i 's/umask 022/umask 077/' "$file"
+done
+
 echo "umask 077" >> /etc/profile
-updatedb
-dnf install -y sysstat puppet rkhunter chkrootkit ghc-cprng-aes
-echo "dev.tty.ldisc_autoload = 0" >> /etc/sysctl.d/90-override.conf
-echo "fs.protected_fifos = 2" >> /etc/sysctl.d/90-override.conf
-echo "fs.protected_regular = 2" >> /etc/sysctl.d/90-override.conf
-echo "fs.suid_dumpable = 0" >> /etc/sysctl.d/90-override.conf
-echo "kernel.dmesg_restrict = 1" >> /etc/sysctl.d/90-override.conf
-echo "kernel.kptr_restrict = 2" >> /etc/sysctl.d/90-override.conf
-echo "kernel.perf_event_paranoid = 3" >> /etc/sysctl.d/90-override.conf
-echo "kernel.sysrq = 0" >> /etc/sysctl.d/90-override.conf
-echo "kernel.unprivileged_bpf_disabled = 1" >> /etc/sysctl.d/90-override.conf
-echo "kernel.yama.ptrace_scope = 1" >> /etc/sysctl.d/90-override.conf
-echo "net.core.bpf_jit_harden = 2" >> /etc/sysctl.d/90-override.conf
-echo "net.ipv4.conf.all.log_martians = 1" >> /etc/sysctl.d/90-override.conf
-echo "net.ipv4.conf.all.rp_filter = 1" >> /etc/sysctl.d/90-override.conf
-echo "net.ipv4.conf.default.log_martians = 1" >> /etc/sysctl.d/90-override.conf
+
+# Update /etc/login.defs to change umask
+sed -i '117s/022/077/g' /etc/login.defs
+
 chmod og-rwx /etc/at.deny
 chmod og-rwx /etc/cron.*
 chmod og-rwx /etc/crontab
-chmod og-rwx /etc/cron.d
 chmod og-rwx /etc/cron.deny
+
+updatedb
+dnf install -y sysstat puppet rkhunter chkrootkit ghc-cprng-aes
+
+# Set sysctl parameters for security
+echo -e "dev.tty.ldisc_autoload = 0\n\
+fs.protected_fifos = 2\n\
+fs.protected_regular = 2\n\
+fs.suid_dumpable = 0\n\
+kernel.dmesg_restrict = 1\n\
+kernel.kptr_restrict = 2\n\
+kernel.perf_event_paranoid = 3\n\
+kernel.sysrq = 0\n\
+kernel.unprivileged_bpf_disabled = 1\n\
+kernel.yama.ptrace_scope = 1\n\
+net.core.bpf_jit_harden = 2\n\
+net.ipv4.conf.all.log_martians = 1\n\
+net.ipv4.conf.all.rp_filter = 1\n\
+net.ipv4.conf.default.log_martians = 1" >> /etc/sysctl.d/90-override.conf
 
 clear
 
@@ -166,21 +168,28 @@ systemctl enable --now dnf-system-upgrade
 clear
 
 # Install and run topgrade.
-cd && wget https://github.com/topgrade-rs/topgrade/releases/download/v11.0.2/topgrade-v11.0.2-x86_64-unknown-linux-gnu.tar.gz
-tar -xf topgrade-v11.0.2-x86_64-unknown-linux-gnu.tar.gz
-install topgrade /usr/local/bin/
-rm -rf topgrade topgrade-v11.0.2-x86_64-unknown-linux-gnu.tar.gz
+topgrade_latest_version=$(curl -s https://api.github.com/repos/topgrade-rs/topgrade/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+topgrade_url="https://github.com/topgrade-rs/topgrade/releases/download/${topgrade_latest_version}/topgrade-${topgrade_latest_version}-x86_64-unknown-linux-gnu.tar.gz"
+topgrade_temp_dir="$(mktemp -d)"
+
+wget "$topgrade_url" -P "$topgrade_temp_dir"
+tar -xf "${topgrade_temp_dir}/topgrade-${topgrade_latest_version}-x86_64-unknown-linux-gnu.tar.gz" -C "$topgrade_temp_dir"
+install "${topgrade_temp_dir}/topgrade" /usr/local/bin/
+rm -rf "$topgrade_temp_dir"
+
 topgrade
 
 clear
 
 # Install grub theme.
-cd && git clone https://github.com/vinceliuice/grub2-themes.git
-cd grub2-themes && ./install.sh -t stylish
-cd && rm -rf grub2-themes
+git clone https://github.com/vinceliuice/grub2-themes.git "$HOME/grub2-themes"
+"$HOME/grub2-themes/install.sh" -t stylish
+rm -rf "$HOME/grub2-themes"
 
 clear
 
 # Speed up systemd journal flush.
-sed -i 's/#Storage=auto/Storage=volatile'/g /etc/systemd/journald.conf
-sed -i 's/#SystemMaxFileSize=/SystemMaxFileSize=20MB'/g /etc/systemd/journald.conf
+sed -i 's/#Storage=auto/Storage=volatile/' /etc/systemd/journald.conf
+sed -i 's/#SystemMaxFileSize=/SystemMaxFileSize=20MB/' /etc/systemd/journald.conf
+
+echo "Cleanup and optimization complete."
