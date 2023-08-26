46317fbd25deebc9627837df4a8ac06dbb59ffa0
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2023-08-26 13:24:28 -0400

    Update bat-setup.sh

--- bat-setup.sh
+++ bat-setup.sh
@@ -30,25 +30,28 @@ esac
 # Generate initial configuration file for bat
 bat --generate-config-file
 
-# Modify the configuration settings
+# Modify the configuration settings for current user.
 sed -i "s/#--theme=\"TwoDark\"/--theme=\"$selected_theme\"/g" "$HOME/.config/bat/config"
 sed -i 's/#--italic-text=always/--italic-text=always/g' "$HOME/.config/bat/config"
 echo '--map-syntax "*.conf:INI"' >> "$HOME/.config/bat/config"
 echo '--map-syntax "config:INI"' >> "$HOME/.config/bat/config"
 
-# Copy the user configuration to /etc/skel so new users get the same setup
+# Copy the user configuration to /etc/skel so new users get the same setup.
 sudo mkdir -p /etc/skel/.config/bat
 sudo cp -v "$HOME/.config/bat/config" /etc/skel/.config/bat/
 
-# Copy the user configuration to root's configuration
+# Copy the user configuration to root's configuration.
 sudo mkdir -p /root/.config/bat
 sudo cp -v "$HOME/.config/bat/config" /root/.config/bat/
 
-# Setup the Catppuccin theme for bat, including root configuration
+# Setup the Catppuccin theme for bat.
 cd "$HOME" || exit
 git clone https://github.com/catppuccin/bat.git
 cd bat
-sudo sh -c 'mkdir -p "$(bat --config-dir)/themes"; cp *.tmTheme "$(bat --config-dir)/themes"; bat cache --build; sed -i "s/1337/$selected_theme/g" "$HOME/.config/bat/config"; sed -i "s/1337/$selected_theme/g" "/root/.config/bat/config"'
+sudo sh -c 'mkdir -p "$(bat --config-dir)/themes"; cp *.tmTheme "$(bat --config-dir)/themes"; bat cache --build'
+
+# Copy themes to /etc/skel.
+sudo sh -c 'mkdir -p /etc/skel/.config/bat/themes; cp *.tmTheme /etc/skel/.config/bat/themes; bat cache --build'
 
 echo "Bat syntax highlighter has been configured with the selected theme ($selected_theme) for both your user and root."
 rm -rf "$HOME/bat"
