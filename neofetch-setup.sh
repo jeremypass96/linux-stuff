a79829b1c48c0e44ff30bb02dfb3aa7b0aac5a76
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2023-08-28 11:20:21 -0400

    Update neofetch-setup.sh

--- neofetch-setup.sh
+++ neofetch-setup.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-mkdir -p $HOME/.config/neofetch
-cp -v $HOME/linux-stuff/Dotfiles/config/neofetch/config.conf $HOME/.config/neofetch/config.conf
-chown $USER:$USER $HOME/.config/neofetch
-chown $USER:$USER $HOME/.config/neofetch/*
+
+# Create directory if it doesn't exist
+mkdir -p "$HOME"/.config/neofetch
+
+# Copy config file to user's config directory
+cp -v "$HOME"/linux-stuff/Dotfiles/config/neofetch/config.conf "$HOME"/.config/neofetch/config.conf
+
+# Set ownership for the user
+chown -R "$USER":"$USER" "$HOME"/.config/neofetch
+
+# Copy config file to system-wide skel directory
 sudo mkdir -p /etc/skel/.config/neofetch
-sudo cp -v $HOME/.config/neofetch/config.conf /etc/skel/.config/neofetch/
+sudo cp -v "$HOME"/.config/neofetch/config.conf /etc/skel/.config/neofetch/
+
+# Copy config file to root's config directory
 sudo mkdir -p /root/.config/neofetch
 sudo cp -v /etc/skel/.config/neofetch/config.conf /root/.config/neofetch/
