84329de4e5d7bfd9331df95fc317f8dde65d0682
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2023-08-28 11:52:41 -0400

    Update zsh-setup.sh

--- zsh-setup.sh
+++ zsh-setup.sh
@@ -5,11 +5,11 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 chsh -s /usr/bin/zsh $USER
 
 # Clone and set up zsh-autosuggestions and zsh-syntax-highlighting plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions.git /home/$USER/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /home/$USER/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 # Copy .zshrc configuration
-cp -v /home/$USER/linux-stuff/Dotfiles/.zshrc /home/$USER/.zshrc
+cp -v $HOME/linux-stuff/Dotfiles/.zshrc $HOME/.zshrc
 
 # Setup Catppuccin syntax highlighting colors
 cd "$HOME" || exit
@@ -23,19 +23,19 @@ echo "Choose Catppuccin colors for syntax highlighting:"
 select scheme in "Latte" "Frappé" "Macchiato" "Mocha"; do
     case $scheme in
         Latte)
-            echo "source /usr/local/etc/zsh/catppuccin_latte-zsh-syntax-highlighting.zsh" >> /home/$USER/.zshrc
+            echo "source /usr/local/etc/zsh/catppuccin_latte-zsh-syntax-highlighting.zsh" >> $HOME/.zshrc
             break
             ;;
         Frappé)
-            echo "source /usr/local/etc/zsh/catppuccin_frappe-zsh-syntax-highlighting.zsh" >> /home/$USER/.zshrc
+            echo "source /usr/local/etc/zsh/catppuccin_frappe-zsh-syntax-highlighting.zsh" >> $HOME/.zshrc
             break
             ;;
         Macchiato)
-            echo "source /usr/local/etc/zsh/catppuccin_macchiato-zsh-syntax-highlighting.zsh" >> /home/$USER/.zshrc
+            echo "source /usr/local/etc/zsh/catppuccin_macchiato-zsh-syntax-highlighting.zsh" >> $HOME/.zshrc
             break
             ;;
         Mocha)
-            echo "source /usr/local/etc/zsh/catppuccin_mocha-zsh-syntax-highlighting.zsh" >> /home/$USER/.zshrc
+            echo "source /usr/local/etc/zsh/catppuccin_mocha-zsh-syntax-highlighting.zsh" >> $HOME/.zshrc
             break
             ;;
         *)
@@ -46,10 +46,10 @@ done
 
 # Clean up and copy configurations for future users and root
 cd && rm -rf zsh-syntax-highlighting
-sudo cp -v /home/$USER/.zshrc /etc/skel/.zshrc
+sudo cp -v $HOME/.zshrc /etc/skel/.zshrc
 sudo cp -v /etc/skel/.zshrc /root/.zshrc
 
 # Add 'pfetch' to .zshrc
-echo "pfetch" >> /home/$USER/.zshrc
+echo pfetch >> $HOME/.zshrc
 
 echo "Zsh and related configurations have been set up with the chosen Catppuccin syntax highlighting."
