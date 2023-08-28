a14c215a53b29f77bc4b38a188501a9306b5d899
Jeremy Passarelli <31392377+jeremypass96@users.noreply.github.com>
2023-08-28 11:17:16 -0400

    Update neofetch-setup.sh

--- neofetch-setup.sh
+++ neofetch-setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-mkdir -p /home/$USER/.config/neofetch
-cp -v /home/$USER/linux-stuff/Dotfiles/config/neofetch/config.conf /home/$USER/.config/neofetch/config.conf
-chown $USER:$USER /home/$USER/.config/neofetch
-chown $USER:$USER /home/$USER/.config/neofetch/*
+mkdir -p $HOME/.config/neofetch
+cp -v $HOME/linux-stuff/Dotfiles/config/neofetch/config.conf $HOME/.config/neofetch/config.conf
+chown $USER:$USER $HOME/.config/neofetch
+chown $USER:$USER $HOME/.config/neofetch/*
 sudo mkdir -p /etc/skel/.config/neofetch
-sudo cp -v /home/$USER/.config/neofetch/config.conf /etc/skel/.config/neofetch/
+sudo cp -v $HOME/.config/neofetch/config.conf /etc/skel/.config/neofetch/
 sudo mkdir -p /root/.config/neofetch
 sudo cp -v /etc/skel/.config/neofetch/config.conf /root/.config/neofetch/
