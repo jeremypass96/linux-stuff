b46b5a80f04ed6c3ff27364917080a175ddc1f4f
Jeremy Passarelli <jeremy.pass.101@aol.com>
2023-09-25 17:12:32 -0400

    Made some adjustments.

--- bat-setup.sh
+++ bat-setup.sh
@@ -45,13 +45,12 @@ sudo mkdir -p /root/.config/bat
 sudo cp -v "$HOME/.config/bat/config" /root/.config/bat/
 
 # Setup the Catppuccin theme for bat.
-cd "$HOME" || exit
-git clone https://github.com/catppuccin/bat.git
-cd bat
-sudo sh -c 'mkdir -p "$(bat --config-dir)/themes"; cp *.tmTheme "$(bat --config-dir)/themes"; bat cache --build'
+cd "$HOME"
+git clone https://github.com/catppuccin/bat.git && cd bat
+sh -c 'mkdir -p "$(bat --config-dir)/themes"; cp *.tmTheme "$(bat --config-dir)/themes"; bat cache --build'
 
 # Copy themes to /etc/skel.
-sudo sh -c 'mkdir -p /etc/skel/.config/bat/themes; cp *.tmTheme /etc/skel/.config/bat/themes; bat cache --build'
+sudo sh -c 'mkdir -p /etc/skel/.config/bat/themes; cp *.tmTheme /etc/skel/.config/bat/themes'
 
 # Modify the configuration settings for root.
 sed -i "s/#--theme=\"TwoDark\"/--theme=\"$selected_theme\"/g" "/root/.config/bat/config"
