b2c209de00b754261dea852f11028d6773c897b3
Jeremy Passarelli <jeremy.pass.101@aol.com>
2023-12-13 00:54:05 -0500

    Added symlink to automatically start PipeWire.

--- void_linux-setup.sh
+++ void_linux-setup.sh
@@ -71,8 +71,9 @@ mkdir -p /etc/pipewire/pipewire.conf.d
 ln -s /usr/share/examples/wireplumber/10-wireplumber.conf /etc/pipewire/pipewire.conf.d/
 ln -s /usr/share/examples/pipewire/20-pipewire-pulse.conf /etc/pipewire/pipewire.conf.d/
 mkdir -p /etc/alsa/conf.d
-ln -s /usr/share/alsa/alsa.conf.d/50-pipewire.conf /etc/alsa/conf.d
-ln -s /usr/share/alsa/alsa.conf.d/99-pipewire-default.conf /etc/alsa/conf.d
+ln -s /usr/share/alsa/alsa.conf.d/50-pipewire.conf /etc/alsa/conf.d/
+ln -s /usr/share/alsa/alsa.conf.d/99-pipewire-default.conf /etc/alsa/conf.d/
+ln -s /usr/share/applications/pipewire.desktop /etc/xdg/autostart/
 
 # Install Newaita icons.
 echo "Installing the Newaita icon theme..."
