db57f26d9cefc857580d8c9da9e1f5184d7b7169
Jeremy Passarelli <jeremy.pass.101@aol.com>
2023-12-13 00:45:11 -0500

    Added ALSA auto configuration to PipeWire setup section.

--- void_linux-setup.sh
+++ void_linux-setup.sh
@@ -70,6 +70,9 @@ vpm install pipewire -y
 mkdir -p /etc/pipewire/pipewire.conf.d
 ln -s /usr/share/examples/wireplumber/10-wireplumber.conf /etc/pipewire/pipewire.conf.d/
 ln -s /usr/share/examples/pipewire/20-pipewire-pulse.conf /etc/pipewire/pipewire.conf.d/
+mkdir -p /etc/alsa/conf.d
+ln -s /usr/share/alsa/alsa.conf.d/50-pipewire.conf /etc/alsa/conf.d
+ln -s /usr/share/alsa/alsa.conf.d/99-pipewire-default.conf /etc/alsa/conf.d
 
 # Install Newaita icons.
 echo "Installing the Newaita icon theme..."
