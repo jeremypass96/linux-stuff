6baceeb5559336a79bb910360f8f7a612f1adcd8
Jeremy Passarelli <jeremy.pass.101@aol.com>
2024-02-09 14:17:34 -0500

    Finally fixed permission denied errors.

--- zsh-setup.sh
+++ zsh-setup.sh
@@ -4,11 +4,32 @@
 sudo ZSH=/usr/share/oh-my-zsh sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 chsh -s /usr/bin/zsh $USER
 
+# Set proper permissions.
+sudo chmod 755 /usr/share/oh-my-zsh
+sudo chmod 755 /usr/share/oh-my-zsh/oh-my-zsh.sh
+sudo chmod 755 /usr/share/oh-my-zsh/cache
+sudo chmod 755 /usr/share/oh-my-zsh/custom
+sudo chmod -R 755 /usr/share/oh-my-zsh/lib
+sudo chmod -R 755 /usr/share/oh-my-zsh/log
+sudo chmod -R 755 /usr/share/oh-my-zsh/plugins
+sudo chmod -R 755 /usr/share/oh-my-zsh/templates
+sudo chmod -R 755 /usr/share/oh-my-zsh/themes
+sudo chmod -R 755 /usr/share/oh-my-zsh/tools
+
 # Clone and set up zsh-autosuggestions and zsh-syntax-highlighting plugins
 ZSH_CUSTOM=/usr/share/oh-my-zsh/custom
 sudo git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
 sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
 unset ZSH_CUSTOM
+sudo chmod 755 /usr/share/oh-my-zsh/custom/plugins/zsh-autosuggestions
+sudo chmod 755 /usr/share/oh-my-zsh/custom/plugins/zsh-autosuggestions/*.zsh
+sudo chmod 755 /usr/share/oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+sudo chmod -R 755 /usr/share/oh-my-zsh/custom/plugins/zsh-syntax-highlighting/docs
+sudo chmod -R 755 /usr/share/oh-my-zsh/custom/plugins/zsh-syntax-highlighting/highlighters
+sudo chmod -R 755 /usr/share/oh-my-zsh/custom/plugins/zsh-syntax-highlighting/images
+sudo chmod -R 755 /usr/share/oh-my-zsh/custom/plugins/zsh-syntax-highlighting/tests
+sudo chmod 755 /usr/share/oh-my-zsh/custom/plugins/zsh-syntax-highlighting/*.zsh
+sudo chmod 755 /usr/share/oh-my-zsh/custom/plugins/zsh-syntax-highlighting/.*
 
 # Copy .zshrc configuration
 cp -v $HOME/linux-stuff/Dotfiles/.zshrc $HOME/.zshrc
@@ -18,6 +39,7 @@ cd
 git clone https://github.com/catppuccin/zsh-syntax-highlighting.git
 sudo mkdir -p /usr/local/etc/zsh
 sudo cp -v zsh-syntax-highlighting/themes/*.zsh /usr/local/etc/zsh
+sudo chmod -R 755 /usr/local/etc
 
 # Choose Catppuccin color scheme for syntax highlighting
 echo "Choose Catppuccin colors for syntax highlighting:"
@@ -52,5 +74,8 @@ sudo cp -v /etc/skel/.zshrc /root/.zshrc
 
 # Add 'pfetch' to .zshrc
 echo pfetch >> $HOME/.zshrc
+
+# Change root's shell.
+chsh -s /usr/bin/zsh root
 
 echo "Zsh and related configurations have been set up with the chosen Catppuccin syntax highlighting."
