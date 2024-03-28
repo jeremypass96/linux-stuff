9c74a996457c80c5fa4ca7fb072502ef5a8050af
Jeremy Passarelli <jeremy.pass.101@aol.com>
2024-03-28 13:44:29 -0400

    Same as the "catppuccino" script, but doesn't install the Catppuccino themes.

---
+++ vim_setup_archlinux.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# This script sets up Vim with the "sorbet" colorscheme and the Lightline plugin for Arch Linux.
+
+# Install Vim.
+sudo pacman -S vim --noconfirm
+
+# Install lightline plugin manager and vim-plug.
+paru -S vim-lightline-git vim-plug --noconfirm
+
+# Configure the vimrc file.
+vimrc_path=/etc/vimrc
+sudo chmod o+x $vimrc_path
+cat << EOF >> $vimrc_path
+set number
+set cursorline
+set smoothscroll
+set termguicolors
+
+let g:lightline = {'colorscheme': 'one'}
+colorscheme sorbet
+set laststatus=2
+set noshowmode
+EOF
+chmod o-x $vimrc_path
