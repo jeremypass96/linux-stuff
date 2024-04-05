a383d309287a07e54c71a61c1026b009498cb650
Jeremy Passarelli <jeremy.pass.101@aol.com>
2024-04-05 15:12:14 -0400

    Added root-checking command.

--- enable-sddm-void.sh
+++ enable-sddm-void.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 # Simple one-linder script to enable and start SDDM after a Void Linux install.
+
+# Checking to see if we're running as root.
+if [ $(id -u) -ne 0 ]; then
+    echo "Please run this script as root via 'sudo'! Thanks."
+    exit
+fi
+
 ln -s /etc/sv/sddm /var/service/sddm
