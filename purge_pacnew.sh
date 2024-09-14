e3bfe6ecd7b7fd852cf8c0359a010cdba9f53d09
Jeremy Passarelli <recordguy96@aol.com>
2024-09-14 13:39:52 -0400

    Improved!

--- purge_pacnew.sh
+++ purge_pacnew.sh
@@ -16,19 +16,37 @@ echo '#!/bin/bash
 # Remove all .pacnew files
 fd -e pacnew . /etc -X rm -rf' | tee "$DEST_DIR/purge_pacnew" > /dev/null
 
-# Content of purge_pacnew.hook
+# Content of check_pacnew script.
+echo '#!/bin/bash
+
+# Search the pacman log for recent warnings about .pacnew files
+log_pacnew=$(grep -P 'warning: .*\.pacnew' /var/log/pacman.log | tail -n 10)
+
+# Verify if .pacnew files exist on the system
+system_pacnew=$(fd -e pacnew . /etc -X rm -rf)
+
+# If there are matching .pacnew files in the system, run the purge script
+if [[ -n "$log_pacnew" && -n "$system_pacnew" ]]; then
+    echo "New .pacnew files detected. Running purge..."
+    /usr/local/bin/purge_pacnew
+else
+    echo "No new .pacnew files detected."
+fi' | tee "$DEST_DIR/check_pacnew" > /dev/null
+
+# Content of check_pacnew.hook
 echo '[Trigger]
 Operation = Install
 Type = Package
 Target = *
 
 [Action]
-Description = Purging .pacnew files...
+Description = Checking for and purging .pacnew files...
 When = PostTransaction
-Exec = /usr/local/bin/purge_pacnew' | tee "$HOOK_DIR/purge_pacnew.hook" > /dev/null
-chmod 644 "$HOOK_DIR/purge_pacnew.hook"
+Exec = /usr/local/bin/check_pacnew' | tee "$HOOK_DIR/check_pacnew.hook" > /dev/null
+chmod 644 "$HOOK_DIR/check_pacnew.hook"
 
-# Make the script executable
+# Make the scripts executable
 chmod 744 "$DEST_DIR/purge_pacnew"
+chmod 744 "$DEST_DIR/check_pacnew"
 
-echo "Installation complete. You can now use the $DEST_DIR/purge_pacnew script and the $HOOK_DIR/purge_pacnew pacman post-install hook."
+echo "Installation complete. You can now use the $DEST_DIR/purge_pacnew and $DEST_DIR/check_pacnew scripts and the $HOOK_DIR/check_pacnew pacman post-install hook."
--- purge_pacsave.sh
+++ purge_pacsave.sh
@@ -10,13 +10,30 @@ fi
 DEST_DIR="/usr/local/bin"
 HOOK_DIR="/etc/pacman.d/hooks"
 
-# Content of purge_pacnew.sh
+# Content of purge_pacsave.sh
 echo '#!/bin/bash
 
 # Remove all .pacsave files
 fd -e pacsave . /etc -X rm -rf' | tee "$DEST_DIR/purge_pacsave" > /dev/null
 
-# Content of purge_pacnew.hook
+# Content of check_pacsave script.
+echo '#!/bin/bash
+
+# Search the pacman log for recent warnings about .pacsave files
+log_pacsave=$(grep -P 'warning: .*\.pacsave' /var/log/pacman.log | tail -n 10)
+
+# Verify if .pacnew files exist on the system
+system_pacsave=$(fd -e pacsave . /etc -X rm -rf)
+
+# If there are matching .pacsave files in the system, run the purge script
+if [[ -n "$log_pacsave" && -n "$system_pacsave" ]]; then
+    echo "New .pacsave files detected. Running purge..."
+    /usr/local/bin/purge_pacsave
+else
+    echo "No new .pacsave files detected."
+fi' | tee "$DEST_DIR/check_pacsave" > /dev/null
+
+# Content of purge_pacsave.hook
 echo '[Trigger]
 Operation = Install
 Type = Package
@@ -25,10 +42,11 @@ Target = *
 [Action]
 Description = Purging .pacsave files...
 When = PostTransaction
-Exec = /usr/local/bin/purge_pacsave' | tee "$HOOK_DIR/purge_pacsave.hook" > /dev/null
-chmod 644 "$HOOK_DIR/purge_pacsave.hook"
+Exec = /usr/local/bin/purge_pacsave' | tee "$HOOK_DIR/check_pacsave.hook" > /dev/null
+chmod 644 "$HOOK_DIR/check_pacsave.hook"
 
 # Make the script executable and fix permissions.
 chmod 744 "$DEST_DIR/purge_pacsave"
+chmod 744 "$DEST_DIR/check_pacsave"
 
 echo "Installation complete. You can now use the $DEST_DIR/purge_pacsave script and the $HOOK_DIR/purge_pacsave pacman post-install hook."
