7642fd850b464a32eed0e141afcb3795b74b81fe
Jeremy Passarelli <recordguy96@aol.com>
2024-09-19 12:46:15 -0400

    SDDM didn't get installed? WTF?

--- void_linux-setup.sh
+++ void_linux-setup.sh
@@ -107,7 +107,7 @@ wget https://github.com/google/fonts/raw/main/ofl/poppins/Poppins-ThinItalic.ttf
 
 # Install KDE.
 echo -e "${GREEN}Installing the KDE desktop...${NC}"
-vpm install kde5 kde5-baseapps kaccounts-integration kaccounts-providers xdg-desktop-portal-kde k3b juk ark kdegraphics-thumbnailers -y
+vpm install kde5 kde5-baseapps kaccounts-integration kaccounts-providers xdg-desktop-portal-kde k3b juk ark kdegraphics-thumbnailers sddm -y
 
 # Enable desktop services.
 echo -e "${BLUE}Enabling desktop services...${NC}"
