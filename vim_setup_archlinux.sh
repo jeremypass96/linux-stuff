8800fc0693f5c1eabfbeee826fc8386e7a97c214
Jeremy Passarelli <recordguy96@aol.com>
2024-11-29 15:40:29 -0500

    Fixed install error.

--- vim_setup_archlinux.sh
+++ vim_setup_archlinux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script sets up Vim with the "one" colorscheme and the Lightline plugin for Arch Linux.
 # install the lightline plugin manager, vim-plug, and colorschemes.
-paru -s vim-lightline-git vim-plug vim-ayu-git vim-auto-pairs-git  --noconfirm
+paru -S vim-lightline-git vim-plug vim-ayu-git vim-auto-pairs-git  --noconfirm
 
 # Configure the vimrc file.
 vimrc_path=/etc/vimrc
