ba4974a0d3f187e85ea60eb38da72655b4ac22c7
Jeremy Passarelli <recordguy96@aol.com>
2024-12-01 15:12:50 -0500

    New post-install script.

---
+++ aurora_post-install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# This script will install the Vimix KDE theme and Papirus icons and install the Brave web browser, VSCodium, and the Pinta image editor on Aurora, and some more useful software.
+
+# Install the Papirus icon theme.
+wget -qO- https://git.io/papirus-icon-theme-install | env DESTDIR="$HOME/.icons" sh
+
+# Install the Vimix KDE theme.
+rpm-ostree install kvantum
+git clone https://github.com/vinceliuice/Vimix-kde.git $HOME/
+cd $HOME/Vimix-kde
+./install.sh -t doder
+
+# Remove Firefox and Kate text editor.
+flatpak uninstall -y firefox kate
+
+# Install the Brave web browser and VSCodium.
+flatpak install -y com.brave.Browser com.vscodium.codium
+
+# Configure VSCodium.
+mkdir -p $HOME/.config/VSCodium/User && cp -v $HOME/linux-stuff/Dotfiles/config/VSCodium/User/settings.json $HOME/.config/VSCodium/User/settings.json
+vscodium --install-extension qyurila.ayu-midas
+vscodium --install-extension jeff-hykin.better-shellscript-syntax
+vscodium --install-extension file-icons.file-icons
+vscodium --install-extension miguelsolorio.fluent-icons
+
+# Install the Pinta image editor.
+flatpak install -y app/com.github.PintaProject.Pinta/x86_64/stable
+
+# Install Audacity.
+flatpak install -y org.audacityteam.Audacity
