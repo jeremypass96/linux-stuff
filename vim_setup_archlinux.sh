cc4979b0215f17cd1ad5e4d49d88cd26a350942a
Jeremy Passarelli <recordguy96@aol.com>
2024-12-22 14:42:46 -0500

    Using "vim-auto-pairs" from the Arch repos instead of the AUR.

--- vim_setup_archlinux.sh
+++ vim_setup_archlinux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script sets up Vim with the "one" colorscheme and the Lightline plugin for Arch Linux.
 # install the lightline plugin manager, vim-plug, and colorschemes.
-paru -S vim-lightline-git vim-plug vim-ayu-git vim-auto-pairs-git  --noconfirm
+paru -S vim-lightline-git vim-plug vim-ayu-git vim-auto-pairs  --noconfirm
 
 # Configure the vimrc file.
 sudo tee -a /etc/vimrc > /dev/null << EOF
