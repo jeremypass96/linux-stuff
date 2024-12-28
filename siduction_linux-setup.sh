d24e86d65c612481800a5a29aa2451ad19a222b4
Jeremy Passarelli <recordguy96@aol.com>
2024-12-28 15:04:07 -0500

    Final adjustments.

--- siduction_linux-setup.sh
+++ siduction_linux-setup.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 # This script will clean up and optimize a Siduction KDE (Debian-based rolling release distribution) installation.
 
-# Add user to sudo group.
-gpasswd -a $USER sudo
-
 # Audio buzz/hum fix.
 echo "options snd-hda-intel power_save=0 power_save_controller=N" | sudo tee /etc/modprobe.d/alsa-base.conf
 
@@ -38,20 +35,40 @@ clear
 sudo nala install audacity vlc fd-find bat lsd vim btop -y
 clear
 
-# Install OneHalf-Dark theme for Xfce terminal.
-sudo mkdir -p /etc/skel/.local/share/xfce4/terminal/colorschemes
-cd $HOME && curl https://raw.githubusercontent.com/sonph/onehalf/master/xfce4-terminal/OneHalfDark.theme -o OneHalfDark.theme
-mkdir -p $HOME/.local/share/xfce4/terminal/colorschemes
-cp -v OneHalfDark.theme /etc/skel/.local/share/xfce4/terminal/colorschemes
-cp -v OneHalfDark.theme $HOME/.local/share/xfce4/terminal/colorschemes
-
+# Install theme(s) for Konsole.
+# Download Konsole colors.
+echo -e "${YELLOW}Downloading Konsole colors...${NC}"
+dialog --title "Konsole Colorscheme" --menu "Which Konsole colorscheme do you want?" 12 40 12 \
+1 "Catppuccin" \
+2 "OneHalf-Dark" \
+3 "Ayu Mirage" 2> /tmp/konsole_resp
+konsole_resp=$(cat /tmp/konsole_resp)
+if [ "$konsole_resp" = 1 ]; then
+    curl --parallel https://raw.githubusercontent.com/catppuccin/konsole/refs/heads/main/themes/catppuccin-frappe.colorscheme -o catppuccin-frappe.colorscheme https://raw.githubusercontent.com/catppuccin/konsole/refs/heads/main/themes/catppuccin-latte.colorscheme -o catppuccin-latte.colorscheme https://raw.githubusercontent.com/catppuccin/konsole/refs/heads/main/themes/catppuccin-macchiato.colorscheme -o catppuccin-macchiato.colorscheme https://raw.githubusercontent.com/catppuccin/konsole/refs/heads/main/themes/catppuccin-mocha.colorscheme -o catppuccin-mocha.colorscheme
+	sudo mkdir -p /usr/share/konsole
+	sudo chmod 755 /usr/share/konsole
+	sudo mv *.colorscheme /usr/share/konsole
+	sudo chmod 644 /usr/share/konsole/*.colorscheme
+elif [ "$konsole_resp" = 2 ]; then
+    curl https://raw.githubusercontent.com/sonph/onehalf/master/konsole/onehalf-dark.colorscheme -o onehalf-dark.colorscheme
+	sudo mkdir -p /usr/share/konsole
+	sudo chmod 755 /usr/share/konsole
+	sudo mv onehalf-dark.colorscheme /usr/share/konsole
+	sudo chmod 644 /usr/share/konsole/onehalf-dark.colorscheme
+elif [ "$konsole_resp" = 3 ]; then
+    curl https://raw.githubusercontent.com/mbadolato/iTerm2-Color-Schemes/refs/heads/master/konsole/Ayu%20Mirage.colorscheme -o AyuMirage.colorscheme
+	sudo mkdir -p /usr/share/konsole
+	sudo chmod 755 /usr/share/konsole
+	sudo mv AyuMirage.colorscheme /usr/share/konsole
+	sudo chmod 644 /usr/share/konsole/AyuMirage.colorscheme
+fi
 clear
 
 # Install and setup Zsh.
-nala install zsh -y && ./zsh-setup.sh
+sudo nala install zsh -y && ./zsh-setup.sh
 
 # Install fastfetch.
-sudo nala install fastfetch -y
+sudo nala install fastfetch -y && ./fastfetch-setup.sh
 
 clear
 
@@ -62,21 +79,18 @@ sudo ./cleanup-systemd-boot.sh
 sudo ln -s /usr/bin/batcat /usr/bin/bat && ./bat-setup.sh
 sudo ./wallpapers.sh
 ./lsd-setup.sh
-./bat-setup.sh
-./zsh-setup.sh
-./fastfetch-setup.sh
 ./vim_setup.sh
 
 # Install Brave web browser.
 sudo nala install curl && sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list > /dev/null
 sudo nala update && sudo nala install brave-browser -y
 
 # Install grub theme.
 echo "Installing the GRUB theme..."
 cd && git clone https://github.com/vinceliuice/grub2-themes.git
 cd grub2-themes && sudo ./install.sh -t stylish
-echo "GRUB_DISABLE_SUBMENU=y" | sudo tee -a /etc/default/grub
+echo "GRUB_DISABLE_SUBMENU=y" | sudo tee -a /etc/default/grub > /dev/null
 cd && rm -rf grub2-themes
 
 # Enable GRUB_DISABLE_RECOVERY in /etc/default/grub.
@@ -85,7 +99,7 @@ sudo sed -i s/#GRUB_DISABLE_RECOVERY/GRUB_DISABLE_RECOVERY/g /etc/default/grub
 sudo chmod o-w /etc/default/grub
 
 # Update environment variables.
-sudo tee -a /etc/environment > /dev/null << EOF
+sudo tee /etc/environment > /dev/null << EOF
 BROWSER=brave
 EDITOR=vim
 EOF
