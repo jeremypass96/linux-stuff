c4b95ee009b96636cd91cceaae757d045ae30bbe
Jeremy Passarelli <recordguy96@aol.com>
2026-04-06 18:25:40 -0400

    Fixed issue.

--- void_linux-setup.sh
+++ void_linux-setup.sh
@@ -116,10 +116,10 @@ sudo ln -s /etc/sv/elogind /var/service/
 # Install and configure PipeWire.
 echo -e "${GREEN}Installing and configuring PipeWire...${NC}"
 sudo vpm install pipewire alsa-pipewire -y
-mkdir -p /etc/pipewire/pipewire.conf.d
+sudo mkdir -p /etc/pipewire/pipewire.conf.d
 sudo ln -s /usr/share/examples/wireplumber/10-wireplumber.conf /etc/pipewire/pipewire.conf.d/
 sudo ln -s /usr/share/examples/pipewire/20-pipewire-pulse.conf /etc/pipewire/pipewire.conf.d/
-mkdir -p /etc/alsa/conf.d
+sudo mkdir -p /etc/alsa/conf.d
 sudo ln -s /usr/share/alsa/alsa.conf.d/50-pipewire.conf /etc/alsa/conf.d/
 sudo ln -s /usr/share/alsa/alsa.conf.d/99-pipewire-default.conf /etc/alsa/conf.d/
 sudo ln -s /usr/share/applications/pipewire.desktop /etc/xdg/autostart/
